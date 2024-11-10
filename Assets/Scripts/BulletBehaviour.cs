17012fd7aca9f4c60b984644cbc69f4930fc8d19
emrefiliz01 <98563077+emrefiliz01@users.noreply.github.com>
2024-11-10 16:51:06 +0300

    Bullet destroy mechanic added

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -13,11 +13,29 @@ public class BulletBehaviour : MonoBehaviour
     void Update()
     {
         transform.position += transform.up * Time.deltaTime * bulletSpeed;
+
     }
 
     public void SetPosition(Vector3 bulletSpawnPoint)
     {
         transform.position = bulletSpawnPoint;
     }
+
+        
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.tag == "BulletDestroyer")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
 }
 
